d621cba8eb776cde7bede23c1ef2ae1d8f09cb0d
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-05-03 13:23:39 -0300

    - Adicionando a biblioteca React Hook Forms (validação de formulário) - Configurando a validação de formulário no componente Formulário

--- aluratube/src/Componentes/Formulario.jsx
+++ aluratube/src/Componentes/Formulario.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { AirtablePOST } from "../API/AirtablePOST";
 import Modal from "react-modal";
+import { useForm, useFormState } from "react-hook-form";
 
 const Formulario = () => {
   const [tituloVideo, setTituloVideo] = useState("");
   const [urlVideo, setUrlVideo] = useState("");
   const [modal, setModal] = useState(false);
+  const [mensagemAdicaoCorreta, setMensagemAdicaoCorreta] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    setErro,
+    formState: { errors },
+  } = useForm();
 
   //verificar se o modal está aberto
   function modalAberto() {
@@ -17,30 +26,36 @@ const Formulario = () => {
     setModal(false);
   }
 
-  const enviar = async (evento) => {
-    evento.preventDefault();
+  const enviar = async () => {
+    try {
+      //converte para uma data no formato ISO 8601 (YYYY-MM-DDTHH:mm:ss.sssZ)
+      const dataAtual = new Date().toISOString();
 
-    //converte para uma data no formato ISO 8601 (YYYY-MM-DDTHH:mm:ss.sssZ)
-    const dataAtual = new Date().toISOString();
+      //os dados dos hooks estão atrelados aos fields do Airtable
+      const data = {
+        title: tituloVideo,
+        url: urlVideo,
+        created_date: dataAtual,
+      };
 
-    //os dados dos hooks estão atrelados aos fields do Airtable
-    const data = {
-      title: tituloVideo,
-      url: urlVideo,
-      created_date: dataAtual,
-    };
+      //limpa os campos do formulário
+      setTituloVideo("");
+      setUrlVideo("");
 
-    //limpa os campos do formulário
-    setTituloVideo("");
-    setUrlVideo("");
+      //pausa e espera pelo retorno da Promisse do Airtable
+      const resultado = await AirtablePOST(data);
 
-    //pausa e espera pelo retorno da Promisse do Airtable
-    const resultado = await AirtablePOST(data);
+      //mensagem de sucesso
+      setMensagemAdicaoCorreta("Vídeo adicionado!");
 
-    if (resultado) {
-      console.log("DEU CERTO", data);
-    } else {
-      console.log("deu errado", data);
+      if (resultado) {
+        console.log("DEU CERTO", data);
+      } else {
+        console.log("deu errado", data);
+      }
+    } catch (erro) {
+      //mensagem de erro
+      setErro("submit", { message: "Erro ao adicionar o vídeo" });
     }
   };
 
@@ -52,20 +67,33 @@ const Formulario = () => {
         <div>
           <button onClick={modalFechado}>x</button>
         </div>
-        <form onSubmit={enviar}>
+        <form onSubmit={handleSubmit(enviar)}>
           <input
             type='text'
             placeholder='nome'
             value={tituloVideo}
+            {...register("titulo", { required: "Titulo é obrigatório" })}
             onChange={(evento) => setTituloVideo(evento.target.value)}
           />
+          {errors.titulo && <p>{errors.titulo.message}</p>}
+
           <input
-            type='text'
+            type='url'
             placeholder='url'
             value={urlVideo}
+            {...register("url", { required: "URL é obrigatório" })}
             onChange={(evento) => setUrlVideo(evento.target.value)}
           />
+          {/* Mensagem de erro */}
+          {errors.url && <p>{errors.url.message}</p>}
+
           <button type='submit'>Enviar</button>
+
+          {/* Mensagem de sucesso */}
+          {mensagemAdicaoCorreta && <p>{mensagemAdicaoCorreta}</p>}
+
+          {/* Mensagem de erro */}
+          {errors.submit && <p>{errors.submit.message}</p>}
         </form>
       </Modal>
     </>
