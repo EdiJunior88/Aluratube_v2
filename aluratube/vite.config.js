573ea2af512292d750aaa82fdf3f8a5fa4dcb51c
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-05-22 21:35:41 -0300

    Reconfigurando o vite.config.js

--- aluratube/vite.config.js
+++ aluratube/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), svgr()],
+    define: {
+      "process.env": env,
+    },
+  };
 });
