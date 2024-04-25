a1b9504caf3e7c706fd56ee45bab1d8e27dfa193
EdiJunior88 <er.moura88@yahoo.com.br>
2024-04-24 22:34:12 -0300

    Update

--- aluratube/.eslintrc.cjs
+++ aluratube/.eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-};
+}
